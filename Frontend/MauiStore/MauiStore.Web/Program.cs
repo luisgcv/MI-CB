68b4e8de552449f940648eebd5b21cbe827a3d29
Yoel <joelputchie@gmail.com>
2026-05-13 20:12:51 -0600

    Eliminar API antigua

--- Frontend/MauiStore/MauiStore.Web/Program.cs
+++ Frontend/MauiStore/MauiStore.Web/Program.cs
@@ -5,6 +5,9 @@ using MauiStore.Web.Services;
 using MudBlazor.Services;
 using Blazored.LocalStorage;
 using MauiStore.Infrastructure;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 using Microsoft.AspNetCore.Components.Authorization;         // AuthorizationCore / AuthStateProvider
 using Microsoft.AspNetCore.Authentication.Cookies;          // CookieAuthenticationDefaults
@@ -94,6 +97,8 @@ builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().Cre
 // Servicio de login/logout
 builder.Services.AddScoped<IAuthService, AuthService>();
 
+
+
 var app = builder.Build();
 
 // Pipeline
@@ -135,68 +140,3 @@ static bool IsApiRequest(HttpRequest req)
 
     return false;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//using MauiStore.Web.Components;
-//using MauiStore.Shared.Services;
-//using MauiStore.Web.Services;
-//using MudBlazor.Services;
-//using Blazored.LocalStorage;
-//using MauiStore.Infrastructure;
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-//builder.Services.AddRazorComponents()
-//    .AddInteractiveServerComponents();
-
-//builder.Services.AddMudServices(); // MudBlazor
-//builder.Services.AddBlazoredLocalStorage();
-//builder.Services.AddScoped<ClientPreferenceManager>();
-
-//// Add device-specific services used by the MauiStore.Shared project
-//builder.Services.AddSingleton<IFormFactor, FormFactor>();
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
-
-//app.UseHttpsRedirection();
-
-//app.UseStaticFiles();
-//app.UseAntiforgery();
-
-//app.MapRazorComponents<App>()
-//    .AddInteractiveServerRenderMode()
-//    .AddAdditionalAssemblies(typeof(MauiStore.Shared._Imports).Assembly);
-
-//app.Run();
